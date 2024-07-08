578958f5e6c88f9b6338db042c9690ae5b1d0d74
WellyngtonMS <wellyngtoncatarino@gmail.com>
2024-07-08 18:33:24 -0300

    fix: navigation implementations for the percentage component and other adjustments

--- src/components/Percent/index.tsx
+++ src/components/Percent/index.tsx
@@ -1,23 +1,34 @@
 import * as S from './styles';
 import { TouchableOpacityProps } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 type Props = TouchableOpacityProps & {
   type: S.ButtonIconTypeStyleProps;
-  showBackButton?: boolean;
+  showBackButton?: S.isBackButtonProps;
   percentage: string;
 };
 
 export function Percent({ type, showBackButton = false, percentage }: Props) {
+  const navigation = useNavigation();
+  
+  function handleOpenStatistics(percentage: string, type: S.ButtonIconTypeStyleProps) {
+    navigation.navigate('statistics', { percentage, type });
+  };
+
+  function handleBack() {
+    navigation.goBack();
+  };
+
   return (
-    <S.Container type={type}>
+    <S.Container type={type} showBackButton={showBackButton}>
       {showBackButton &&
-        <S.BackButton onPress={() => {}}>
+        <S.BackButton onPress={handleBack}>
           <S.BackIcon type={type} />
         </S.BackButton>
       }
       <S.Content>
         {!showBackButton && 
-          <S.Button>
+          <S.Button onPress={() => handleOpenStatistics(percentage, type)}>
             <S.Icon type={type} />
           </S.Button>
         }
--- src/components/Percent/styles.ts
+++ src/components/Percent/styles.ts
@@ -1,18 +1,21 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { ArrowUpRight, ArrowLeft } from 'phosphor-react-native';
 import { TouchableOpacity } from 'react-native';
 
-export type ButtonIconTypeStyleProps = 'PRIMARY' | 'SECONDARY';
+export type ButtonIconTypeStyleProps = 'GOOD' | 'BAD';
+export type isBackButtonProps = boolean;
 
 type Props = {
   type: ButtonIconTypeStyleProps;
+  showBackButton?: isBackButtonProps;
 };
 
 export const Container = styled.View<Props>`
   width: 100%;
-  background-color: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.GREEN_300 : theme.COLORS.RED_300};
+  background-color: ${({ theme, type }) => type === 'GOOD' ? theme.COLORS.GREEN_300 : theme.COLORS.RED_300};
   border-radius: 8px;
-  padding: 20px 16px;
+  padding: ${({ showBackButton }) => showBackButton ? '12px 24px' : '20px 16px'};
+  margin-bottom: 36px;
 `;
 
 export const Content = styled.View`
@@ -22,15 +25,15 @@ export const Content = styled.View`
 `;
 
 export const Button = styled(TouchableOpacity)`
-  width: 100%;
   flex-direction: row;
   justify-content: flex-end;
   position: absolute;
   top: 0;
+  right: 0;
 `;
 
 export const Icon = styled(ArrowUpRight).attrs<Props>(({ theme, type }) => ({
-  color: type === 'PRIMARY' ? theme.COLORS.GREEN_100 : theme.COLORS.RED_100,
+  color: type === 'GOOD' ? theme.COLORS.GREEN_100 : theme.COLORS.RED_100,
   size: 24,
 }))``;
 
@@ -39,18 +42,22 @@ export const BackButton = styled.TouchableOpacity`
 `;
 
 export const BackIcon = styled(ArrowLeft).attrs<Props>(({ theme, type }) => ({
-  color: type === 'PRIMARY' ? theme.COLORS.GREEN_100 : theme.COLORS.RED_100,
+  color: type === 'GOOD' ? theme.COLORS.GREEN_100 : theme.COLORS.RED_100,
   size: 24,
 }))``;
 
 export const Title = styled.Text`
-  font-size: ${({ theme }) => theme.FONT_SIZE.XXL};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
-  color: ${({ theme }) => theme.COLORS.GRAY_100};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.XXL}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_100};
+  `}
 `;
 
 export const Subtitle = styled.Text`
-  font-size: ${({ theme }) => theme.FONT_SIZE.SM};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  color: ${({ theme }) => theme.COLORS.GRAY_200};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.GRAY_200};
+  `}
 `;
